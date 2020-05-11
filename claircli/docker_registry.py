c4e62fda08d78f668bbe71e30f408b630674b22b
Groboclown <matt@groboclown.net>
2020-05-11 14:03:29 -0500

    Fixed formatting issues with checkin.

--- claircli/docker_registry.py
+++ claircli/docker_registry.py
@@ -82,8 +82,11 @@ class RemoteRegistry(object):
         return self.domain
 
     def get_auth(self, repository):
-        if not self.tokens[self.domain].get(repository) and self.tokens[self.domain].get(''):
-            self.tokens[self.domain][repository] = 'Basic ' + self.tokens[self.domain].get('')
+        if (
+                not self.tokens[self.domain].get(repository) and self.tokens[self.domain].get('')
+        ):
+            self.tokens[self.domain][repository] = \
+                'Basic ' + self.tokens[self.domain].get('')
         elif not self.tokens[self.domain].get(repository):
             resp = request('GET', self.url)
             if resp.status_code not in (200, 401):
